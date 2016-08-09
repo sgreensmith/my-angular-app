25e581e4a958d6f28980a08214b961f053c231c6
Simon <simongreensmith.office@gmail.com>
2016-08-09 22:13:05 +0100

    WIP creating  and  features

--- src/scope.js
+++ src/scope.js
@@ -3,13 +3,15 @@
 var _ = require('lodash');
 
 function Scope() {
-    this.$$watchers = [];
+    this.$$watchers = [];   // $$ makes the property private to angular
 
     // set up a $watch function that binds a watchFn and listenerFn to the array of items in the scope
     Scope.prototype.$watch = function (watchFn, listenerFn) {
         var watcher = {
             watchFn: watchFn,
-            listenerFn: listenerFn
+            listenerFn: listenerFn || function() {},
+            last: initWatchVal  // set an undefined function to the initial 'last' value. this is then reset by the
+                                // $digest function to either equal the old or new value appropriately
         };
         this.$$watchers.push(watcher);
     };
@@ -17,11 +19,20 @@ function Scope() {
     // set up a $digest function that calls every items listenerFn
     Scope.prototype.$digest = function() {
         var self = this;
-        _.forEach(this.$$watchers, function(watcher) {
-            watcher.watchFn(self);
-            watcher.listenerFn();
+        var newValue, oldValue;
+        _.forEach(this.$$watchers, function (watcher) {
+            newValue = watcher.watchFn(self);
+            oldValue = watcher.last;
+            if (newValue !== oldValue) {
+                watcher.last = newValue;
+
+                watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);  // the ternary
+                        // operator resets watch.last IFF if the first time digest() has been called
+            }
         });
     };
 }
+
+function initWatchVal() { }
 
 module.exports = Scope;
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -37,5 +37,77 @@ describe('Scope', function() {
 
             expect(watchFn).toHaveBeenCalledWith(scope);
         });
+
+        it('calls the listener function when the watched value changes', function() {
+
+            scope.someValue = 'a';
+            scope.counter = 0;
+
+            scope.$watch(
+                function(scope) {
+                    return scope.someValue;
+                },  // watchFn
+                function(newValue, oldValue, scope) {
+                    scope.counter++;
+                }  // listenerFn
+            );
+
+            expect(scope.counter).toBe(0);
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+
+            scope.someValue = 'b';
+            expect(scope.counter).toBe(1);
+
+            scope.$digest();
+            expect(scope.counter).toBe(2);
+
+        });
+
+        it('calls listener when watch value is first undefined', function() {
+
+            scope.counter = 0;
+
+            scope.$watch(
+                function(scope) { return scope.someValue; },
+                function(newValue, oldValue, scope) {
+                    scope.counter++;
+                }
+            );
+
+            scope.$digest();
+
+            expect(scope.counter).toBe(1);
+
+        });
+
+        it('calls listener with the new value as old value for the first time', function() {
+
+            scope.someValue = 123;
+            var oldValueGiven;
+
+            scope.$watch(
+                function(scope) { return scope.someValue; },
+                function(newValue, oldValue, scope) {
+                    oldValueGiven = oldValue;
+                }
+            );
+
+            scope.$digest();
+            expect(oldValueGiven).toBe(123);
+        });
+
+        it('may have watchers that omit the listener function', function() {
+
+            var watchFn = jasmine.createSpy().and.returnValue('something');
+            scope.$watch(watchFn);
+
+            scope.$digest();
+            expect(watchFn).toHaveBeenCalled();
+        });
     });
 });
